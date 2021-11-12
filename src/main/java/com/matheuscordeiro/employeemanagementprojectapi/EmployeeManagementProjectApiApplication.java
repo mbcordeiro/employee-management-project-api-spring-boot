753fbfb60ce5a30b93b1481a017227dacaa1db6c
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-12 02:12:01 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/employeemanagementprojectapi/EmployeeManagementProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.employeemanagementprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/employeemanagementprojectapi/EmployeeManagementProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.employeemanagementprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
